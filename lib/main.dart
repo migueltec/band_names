e5a08d531ef8f1b02ac365c08b35b9ecae5af761
Miguel Tec <li.migueltec@gmail.com>
2020-09-08 06:03:57 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Band name',
+            initialRoute: 'home',
+            routes: {
+                'home': (_)=>HomePage(),
+            },
+        );
+    }
+}
